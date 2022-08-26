9031dbab8f04fb3c965259cd8d9a869bd68c05f6
Brayden Blackwell <braydenblackwell42@gmail.com>
2022-08-26 03:05:33 -0700

    feat: no search results text for product search

--- src/sections/products/ProductList.js
+++ src/sections/products/ProductList.js
@@ -1,17 +1,25 @@
 import PropTypes from "prop-types";
-import { Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import ProductCard from "./ProductCard";
 
 export default function ProductList({ products, ...other }) {
   return (
     <Grid container spacing={3} {...other}>
-      <>
-        {products.map((product) => (
-          <Grid key={product.id} item xs={12} sm={6} md={4}>
-            <ProductCard product={product} />
-          </Grid>
-        ))}
-      </>
+      {products.length ? (
+        <>
+          {products.map((product) => (
+            <Grid key={product.id} item xs={12} sm={6} md={4}>
+              <ProductCard product={product} />
+            </Grid>
+          ))}
+        </>
+      ) : (
+        <Box sx={{ width: "100%", mt: 4 }}>
+          <Typography variant="h5" sx={{ textAlign: "center" }}>
+            No search results found.
+          </Typography>
+        </Box>
+      )}
     </Grid>
   );
 }
